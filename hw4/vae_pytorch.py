e73adfcfc8851b141f9c0a3cfedb932e399f5929
ShannonKuo <b03901009@ntu.edu.tw>
2018-05-09 14:06:13 +0800

    add plot figures

--- hw4/vae_pytorch.py
+++ hw4/vae_pytorch.py
@@ -9,7 +9,8 @@ from torchvision.utils import save_image
 import torchvision.transforms.functional as F
 import scipy.misc
 import os
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 def to_img(x):
     x = 0.5 * (x + 1)
@@ -17,15 +18,19 @@ def to_img(x):
     x = x.view(x.size(0), 3, 64, 64)
     return x
 
-debug = 0
+debug = 1
 if debug == 1:
-    num_epochs = 1
-else:
     num_epochs = 3
+else:
+    num_epochs = 30
 batch_size = 32
 learning_rate = 1e-4
 output_folder = './output'
 test_output_folder = './test_output'
+output_fig_folder = './output_fig'
+MSEloss = []
+KLDloss = []
+
 img_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -88,7 +93,10 @@ class autoencoder(nn.Module):
         self.fcn12 = nn.Linear(1024, 1024)
         #self.conv11 = nn.Conv2d(8, 4, 3, stride=2, padding=2) # 4, 8, 8
         #self.conv12 = nn.Conv2d(8, 4, 3, stride=2, padding=2)
-
+    def random_generate(self, vector):
+        vector = vector.view(-1, 4, 16, 16)
+        z = self.decoder(vector)
+        return z
     def encode(self, x):
         x = self.encoder(x)
         x = x.view(-1, x.size()[1] * x.size()[2] * x.size()[3])
@@ -125,14 +133,17 @@ def loss_function(recon_x, x, mu, logvar):
     logvar: latent log variance
     """
     criterion = nn.MSELoss()
-    BCE = criterion(recon_x, x)  # mse loss
+    MSE = criterion(recon_x, x)  # mse loss
     # loss = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
-    KLD = torch.sum(KLD_element).mul_(-0.5)
+    KLD = torch.sum(KLD_element).mul_(-0.5).div_(batch_size)
     # KL divergence
     lambdaKL = 1e-7
     KLD = KLD.mul_(lambdaKL)
-    return BCE + KLD
+    MSEloss.append(MSE.data[0])
+    KLDloss.append(KLD.data[0])
+
+    return MSE + KLD
 
 def training(data_loader, file_list):
     print("start training")
@@ -141,15 +152,14 @@ def training(data_loader, file_list):
     else:
         model = autoencoder().cpu()
     model.train()
-    #print(summary((3, 64, 64), model))
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
                                 weight_decay=1e-5)
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
+    all_loss = []
     for epoch in range(num_epochs):
         idx = 0
-        #id = 0
         train_loss = 0.0
         for img in data_loader:
             if args.cuda:
@@ -157,17 +167,9 @@ def training(data_loader, file_list):
             else:
                 img = Variable(img).cpu()
             img_show = to_img(img.cpu().data)
-            """if not os.path.exists('./tmp'):
-                os.makedirs('./tmp')
-            for i in range(len(img_show)):
-                save_image(img_show[i], './tmp/' + file_list[id], normalize=True)
-                id += 1
-            """
             # ===================forward=====================
             output, mu, logvar = model(img)
-            #output = model(img)
             loss = loss_function(output, img, mu, logvar)
-            #loss = nn.MSELoss()(output, img)
             # ===================backward====================
             optimizer.zero_grad()
             loss.backward()
@@ -191,22 +193,80 @@ def testing(model, data_loader, file_list):
     model.eval()
     if not os.path.exists(test_output_folder):
         os.makedirs(test_output_folder)
+    if not os.path.exists(output_fig_folder):
+        os.makedirs(output_fig_folder)
     idx = 0
+    test_loss = 0.0
+    ten_images = [0 for x in range(20)]
+    choose_cnt = 0
     for i, img in enumerate(data_loader):
         if args.cuda:
             img = Variable(img).cuda()
         else:
             img = Variable(img).cpu()
         output, mu, logvar = model(img)
-        #output = model(img)
         pic = to_img(output.cpu().data)
+        loss = nn.MSELoss()(output, img)
+        test_loss += loss.data[0] / len(data_loader)
+
         for j in range(len(pic)):
             file_path = test_output_folder + '/' + file_list[idx]
             save_image(pic[j], test_output_folder + '/' + file_list[idx], normalize=True)
             idx += 1
+            if choose_cnt < 10:
+                ten_images[choose_cnt] = img[j]
+                ten_images[10 + choose_cnt] = pic[j]
+                choose_cnt += 1
+
+    out = torchvision.utils.make_grid(ten_images, nrow=10)
+    save_image(out, output_fig_folder + '/fig1_3.jpg', normalize=True)
+
+def random_generate_img(model):
+    model.eval()
+    images = []
+    if not os.path.exists(output_fig_folder):
+        os.makedirs(output_fig_folder)
+
+    for i in range(32):
+        x = torch.randn(1024)
+        output = model.random_generate(x)
+        pic = to_img(output.cpu().data)
+        images.append(pic[0])
+    out = torchvision.utils.make_grid(images, nrow=8)
+    save_image(out, output_fig_folder + '/fig1_4.jpg', normalize=True)
+
+
+def plot_loss():
+    if not os.path.exists(output_fig_folder):
+        os.makedirs(output_fig_folder)
+
+    fig=plt.figure(figsize=(15, 5))
+    t = np.arange(0.0, num_epochs, 1.0)
+    fig.add_subplot(1, 2, 1)
+    line, = plt.plot(t, MSEloss, lw=2)
+    plt.xlabel('epochs')
+    plt.ylabel('MSE_loss')
+    plt.title('MSE_loss vs epochs')
+    plt.ylim(-2,2)
+
+    t = np.arange(0.0, num_epochs, 1.0)
+    fig.add_subplot(1, 2, 2)
+    line, = plt.plot(t, KLDloss, lw=2)
+    plt.xlabel('epochs')
+    plt.ylabel('KLD_loss')
+    plt.title('KDL_loss vs epochs')
+    plt.ylim(-2,2)
+
+    plt.savefig(output_fig_folder + '/fig1_2.jpg')
+    plt.close()
 
 if __name__ == '__main__':
+    torch.manual_seed(999)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(999)
     train_data_loader, train_file_list = load_image('./hw4_data/train')
     test_data_loader, test_file_list = load_image('./hw4_data/test')
     model = training(train_data_loader, train_file_list)
     testing(model, test_data_loader, test_file_list)
+    random_generate_img(model)
+    plot_loss()
